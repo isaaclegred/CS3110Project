a4c0148ea86fb9add2dc3442149629ffe44fdd86
Arthur Maximilian Tanjaya <amt333@cornell.edu>
2020-04-26 05:53:25 -0400

    Implemented oneLayer

--- src/oneLayer.ml
+++ src/oneLayer.ml
@@ -2,13 +2,28 @@ open Trainer
 
 module Make (In : Data) (Out : Data) = struct
 
+  module M = Owl.Mat
   module In = In
   module Out = Out
 
   type t = unit
 
+  let layer =
+    Layer.create
+      (M.uniform In.size Out.size)
+      (M.zeros Out.size 1)
+      (List.init Out.size (fun _ -> Owl.Maths.sigmoid))
+
   let create ins outs =
-    Network.create In.size Out.size
-    |> ignore
+    Network.(
+      create In.size Out.size
+      |> seal layer
+    )
+
+  let cost expected actual =
+    let to_mat arr = M.of_array arr (Array.length arr) 1 in
+    let expected = expected |> Out.to_float_array |> to_mat in
+    let actual = actual |> to_mat in
+    M.(expected - actual |> sqr |> sum')
 
 end
