142683a616b1f9672afa6d14546eb44c6a26c98f
Arthur Maximilian Tanjaya <amt333@cornell.edu>
2020-04-25 21:30:44 -0400

    Added layer module

---
+++ src/layer.mli
@@ -0,0 +1,27 @@
+type t
+
+val create : Owl.Mat.mat -> Owl.Mat.mat -> (float -> float) list -> t
+
+val input_size : t -> int
+
+val layer_size : t -> int
+
+val get_weights : t -> Owl.Mat.mat
+
+val set_weights : t -> Owl.Mat.mat -> t
+
+val get_biases : t -> Owl.Mat.mat
+
+val set_biases : t -> Owl.Mat.mat -> t
+
+val get_activation : t -> (float -> float) list
+
+val set_activation : t -> (float -> float) list -> t
+
+val run : Owl.Mat.mat -> t -> Owl.Mat.mat
+
+val to_string : t -> string
+
+val from_string : string -> t
+
+val copy : t -> t
