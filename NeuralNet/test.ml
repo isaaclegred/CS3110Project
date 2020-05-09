50ee340db1ac707d119da572d6bb3c69f761804a
Isaac Legred <inl7@cornell.edu>
2020-05-09 16:13:55 -0400

    add a little test structure

--- NeuralNet/test.ml
+++ NeuralNet/test.ml
@@ -1,3 +1,17 @@
 open OUnit
 
+  
+
+exception IOFailure of string
+    
 let test_file = IO.(read "./test.csv" R)
+let test_params_holder = IO.(make_blank_params_file "F_Params.csv" RW)
+let test_pf = IO.(unpack_params test_params_holder)
+let params =  match test_pf with
+    | None -> IOFailure "No parameters where extracted from the csv" |> raise
+    | Some pf -> !(pf.params)
+  
+
+let io_tests = [ 
+  
+]
