f004d5738304e64df176e4213dc1fafe98953ef7
Isaac Legred <inl7@cornell.edu>
2020-05-10 16:01:06 -0400

    added a test that fails

--- NeuralNet/test.ml
+++ NeuralNet/test.ml
@@ -2,16 +2,27 @@ open OUnit2
 module Mat = Owl.Mat
 
 let string_of_mat = Owl_pretty.dsnda_to_string
-let make_mat_equality_test name expected actual : test =
+let cmp_mat_arr name (exp_arr, rows, columns) actual_mat : test =
+  let exp_mat = Mat.of_array exp_arr rows columns in
+  name >::(fun _ ->
+      assert_equal exp_mat actual_mat ~printer:string_of_mat)
+let cmp_mats name expected actual : test =
   name >::(fun _ ->
       assert_equal expected actual ~printer:string_of_mat)
-  (* IO Tests *)
+
+(* IO Tests *)
 
 open IO
 
 exception IOFailure of string
 
-let test_file = read "./test.csv" R
+let raw_test_file = read "./test.csv" R
+let data_file = unpack_data raw_test_file
+let data = match data_file with
+  | None -> IOFailure "No data was extracted" |> raise
+  | Some df -> match df.data with
+    | None ->  IOFailure "No data was extracted" |> raise
+    | Some local_data -> local_data
 let test_params_holder = make_blank_params_file "F_Params.csv" RW
 let test_pf = unpack_params (Some test_params_holder)
 let params =
@@ -22,7 +33,7 @@ let params =
 
   
 let io_tests = [
-
+cmp_mat_arr "independent_data test"  ([|1.; 2.; 3.|], 3, 1) (fst data)
 ]
 
 (* Layer tests *)
