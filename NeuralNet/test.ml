985978fe32e3b2d86c44178025ae4ac463e58491
Arthur Maximilian Tanjaya <amt333@cornell.edu>
2020-05-10 06:57:15 -0400

    Add test skeleton

--- NeuralNet/test.ml
+++ NeuralNet/test.ml
@@ -1,17 +1,57 @@
-open OUnit
+open OUnit2
 
-  
+(* IO Tests *)
+
+open IO
 
 exception IOFailure of string
-    
-let test_file = IO.(read "./test.csv" R)
-let test_params_holder = IO.(make_blank_params_file "F_Params.csv" RW)
-let test_pf = IO.(unpack_params test_params_holder)
-let params =  match test_pf with
-    | None -> IOFailure "No parameters where extracted from the csv" |> raise
-    | Some pf -> !(pf.params)
-  
 
-let io_tests = [ 
-  
+let test_file = read "./test.csv" R
+let test_params_holder = make_blank_params_file "F_Params.csv" RW
+let test_pf = unpack_params (Some test_params_holder)
+let params =
+  match test_pf with
+  | None -> IOFailure "No parameters were extracted from the csv" |> raise
+  | Some pf -> !(pf.params)
+
+let io_tests = [
+
 ]
+
+(* Layer tests *)
+
+let layer_tests = [
+
+]
+
+(* Network tests *)
+
+let network_tests = [
+
+]
+
+(* Derivative tests *)
+
+let derivative_tests = [
+
+]
+
+(* DefaultTrainer tests *)
+
+let default_trainer_tests = [
+
+]
+
+(* Run tests *)
+
+let tests = [
+  io_tests;
+  layer_tests;
+  network_tests;
+  derivative_tests;
+  default_trainer_tests;
+]
+
+let suite = "project test suite" >::: List.flatten tests
+
+let _ = run_test_tt_main suite
